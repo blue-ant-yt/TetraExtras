83d5cbb49d863e616ed7aa0e8aecbb394ca101cb
Vinnaxy <iainkrukenberg65@gmail.com>
2023-10-22 02:27:34 -0400

    Initial commit

---
+++ src/main/java/net/vinnaxy/tetraextrassro/TetraExtrasSro.java
@@ -0,0 +1,40 @@
+package net.vinnaxy.tetraextrassro;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TetraExtrasSro.MOD_ID)
+public class TetraExtrasSro {
+    public static final String MOD_ID = "tetraextrassro";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public TetraExtrasSro() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
