9e18c2a221e41161f539f38815622d6dfd01c792
Vinnaxy <iainkrukenberg65@gmail.com>
2023-10-29 22:57:35 -0400

    Update Log
    Version 0.0.8
    Added wither bones into the game can't get them yet, but it's added into the game.
    Added compatibility with Enigmatic Legacy.
    Added spanish translations.

--- src/main/java/net/vinnaxy/tetraextrassro/TetraExtrasSro.java
+++ src/main/java/net/vinnaxy/tetraextrassro/TetraExtrasSro.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.vinnaxy.tetraextrassro.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -18,6 +19,8 @@ public class TetraExtrasSro {
 
     public TetraExtrasSro() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.ITEMS.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
