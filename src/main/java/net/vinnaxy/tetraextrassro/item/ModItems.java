9ac11d7cbb57cedf8d24d62b173332de0f031567
Vinnaxy <iainkrukenberg65@gmail.com>
2023-11-22 00:32:10 -0500

    Update Log
    Version 0.1.0
    Fixed a crash hopefully and remove some stuff due to crashes.
    Removed the Wither Bone the mod added for 1.19 it was not in 1.20 so everything is the same there.

--- src/main/java/net/vinnaxy/tetraextrassro/item/ModItems.java
+++ src/main/java/net/vinnaxy/tetraextrassro/item/ModItems.java
@@ -12,8 +12,6 @@ public class ModItems {
    public static final DeferredRegister<Item> ITEMS =
            DeferredRegister.create(ForgeRegistries.ITEMS, TetraExtrasSro.MOD_ID);
 
-    public static final RegistryObject<Item> WITHER_BONE = ITEMS.register("wither_bone",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
    public static void register(IEventBus eventBus) {
        ITEMS.register(eventBus);
